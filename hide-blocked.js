d53cd4ace2a886e97f2ddaffc2c5ae6696861121
TacoConKvass <e_frun@o2.pl>
2025-11-16 20:58:22 +0000

    Add: Implement functionality

---
+++ hide-blocked.js
@@ -0,0 +1,14 @@
+const style = document.createElement('style');
+style.type = "text/css"
+const css = `
+.groupStart__5126c:has(div[class^="blockedSystemMessage"]) {
+  height: 0px;
+  margin: 0px;
+  opacity: 0%;
+  color: transparent;
+}
+`;
+
+style.appendChild(document.createTextNode(css));
+
+document.head.appendChild(style);
