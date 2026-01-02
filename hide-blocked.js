69b28b8ddb46ff765f980556e9e59af91bac4a7b
TacoConKvass <e_frun@o2.pl>
2026-01-02 14:56:06 +0100

    Fix: Use the proper wildcard selector

--- hide-blocked.js
+++ hide-blocked.js
@@ -1,11 +1,8 @@
 const style = document.createElement('style');
 style.type = "text/css"
 const css = `
-div[class^="groupStart"]:has(div[class^="blockedSystemMessage"]) {
-  height: 0px;
-  margin: 0px;
-  opacity: 0%;
-  color: transparent;
+[class*="groupStart"]:has([class*="blockedSystemMessage"]) {
+  display: none;
 }
 `;
 
